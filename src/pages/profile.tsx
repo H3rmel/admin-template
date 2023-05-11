d911f31755bd123ae02ec0f80a934c915a2e2e26
Isaac Hermel <isaachermel@gmail.com>
2023-05-10 22:28:27 -0300

    new profile page

---
+++ src/pages/profile.tsx
@@ -0,0 +1,13 @@
+import { MainLayout } from "@/components/Index";
+
+export default function Settings() {
+  return (
+    <MainLayout
+      pageTitle="Perfil"
+      title="Perfil"
+      subtitle="Gerencie seu perfil!"
+    >
+      <p>Configurações</p>
+    </MainLayout>
+  );
+}
